f1eb3d09c032fab2bff064f7c72ff4ea856b0fa6
audrasjb <[audrasjb@gmail.com]>
2016-11-29 21:48:49 +0100

    original commit

---
+++ includes/class-asagenda-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://jeanbaptisteaudras.com
+ * @since      1.0.0
+ *
+ * @package    Asagenda
+ * @subpackage Asagenda/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Asagenda
+ * @subpackage Asagenda/includes
+ * @author     audrasjb <audrasjb@gmail.com>
+ */
+class Asagenda_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/partials/asagenda-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       http://jeanbaptisteaudras.com
+ * @since      1.0.0
+ *
+ * @package    Asagenda
+ * @subpackage Asagenda/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
