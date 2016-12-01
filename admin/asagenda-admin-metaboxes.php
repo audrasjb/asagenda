5b96ae28583147ac62f7f8b07afcbcbbb3cdb888
audrasjb <[audrasjb@gmail.com]>
2016-12-01 18:20:46 +0100

    adding GoogleMaps metabox

--- admin/asagenda-admin-metaboxes.php
+++ admin/asagenda-admin-metaboxes.php
@@ -107,5 +107,64 @@
 
 
 	function asagenda_Create_Place_Metabox( $post ) {
+    	// Use nonce for verification
+		// wp_nonce_field( plugin_basename( __FILE__ ), 'post_map_noncename' );
+		?>
+		<table border="0" align="center" cellpadding="5" cellspacing="0" class="customfieldtable">
+			<tr>
+				<td align="right">Country or Region<br />
+					(This is to GeoCode Map the location using Google)</td>
+				<td><input name="geoecode_country" type="text" value="<?php echo get_post_meta($post->ID, 'asagenda_place', true); ?>" size="30" /></td>
+				<td rowspan="3">&nbsp;</td>
+				<td width="400" rowspan="4" valign="top">
+					Please enter country and update to preview Google Map
+				<div id="asagenda_map_canvas" style="height: 300px; width: 100%;"></div>
+			</tr>
+    </table>
+    <?php
+	}	
 	
-	}
+	function print_google_map_script() {
+        // we could load conditionally by page if we want here
+        global $post;
+		$geocode_country = get_post_meta($post->ID, 'asagenda_place', true);
+        ?>
+        <script type='text/javascript'>
+		var geocoder;
+        var map;
+        var query = '<?php echo $geocode_country; ?>';
+        function initialize() {
+        	geocoder = new google.maps.Geocoder();
+			var mapOptions = {
+            	zoom:3,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+            	}
+				map = new google.maps.Map(document.getElementById('asagenda_map_canvas'), mapOptions);
+				codeAddress();
+        	}
+			function codeAddress() {
+            	var address = query;
+				geocoder.geocode( { 'address': address}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					map.setCenter(results[0].geometry.location);
+					var marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+                	});
+            	} else {
+					alert('Geocode was not successful for the following reason: ' + status);
+            	}
+            });
+        }
+		function loadScript() {
+        	var script = document.createElement("script");
+			script.type = "text/javascript";
+			script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCLDpLStQGls1VjreRde24wRKs9QV4Gq3g";
+			document.body.appendChild(script);
+       	}
+	   	window.onload = loadScript;   
+        </script>
+        <?php
+    }
+    add_action('admin_head', 'print_google_map_script');
+   
