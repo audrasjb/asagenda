47701797794744d31dfc44366347dc1aca38a073
audrasjb <[audrasjb@gmail.com]>
2016-12-12 19:59:50 +0100

    tinymce button for shortcodes"

---
+++ admin/asagenda-admin-shortcodes-tinymce.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The specifics admin tinymce button for generate the shortcodes of the plugin.
+ *
+ * @link       http://jeanbaptisteaudras.com
+ * @since      1.0.0
+ *
+ * @package    Asagenda
+ * @subpackage Asagenda/admin
+ */
+
+/**
+ * The specifics admin tinymce button for generate the shortcodes of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Asagenda
+ * @subpackage Asagenda/admin
+ * @author     audrasjb <audrasjb@gmail.com>
+ */
+   
+ 
